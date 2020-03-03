4665a8287ee4c0d7ef169530429decb07fae11e3
amir <amir.hussain1@ucalgary.ca>
2020-03-03 11:42:14 -0700

    Loggin complete Clean up still required

--- src/logic/User.java
+++ src/logic/User.java
@@ -1,10 +1,28 @@
 package logic;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class User {
 
 	//checks to see if the user exists or if the information is correct
-	public String Authenticate(int type,String username,String Password) {
-
+	public String Authenticate(String username,String password) {
+		File file = new File(username);
+		try {
+			Scanner scan = new Scanner(file);
+//			System.out.println(scan.nextLine());
+//			System.out.println(scan.nextLine());
+			scan.nextLine();
+			scan.nextLine();
+			if(scan.nextLine().equals(password)) {
+				return "true";
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("DASFAS");
+			return "DNE";
+		}
+		
 		return "false";
 	}
 
